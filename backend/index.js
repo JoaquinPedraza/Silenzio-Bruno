913e94ffa925b708ce0dd069695a1c1728201aff
Andrea Sofia Abraham Vieyra <drudruabraham@gmail.com>
2022-03-17 15:03:35 -0300

    agregué la parte de los funkos

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require ('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const User = require('./models/userModel');
+const Funko = require ('./models/funkoModel');
 const userRouter = require('./routers/userRoutes')(User);
+const funkoRouter = require ('./routers/funkoRouter')(Funko);
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/APIweb');
+mongoose.connect('mongodb://127.0.0.1:27017/funkosAPI');
 
-app.use('/api', userRouter);
+app.use('/api', userRouter,funkoRouter);
 app.listen(8080, ()=> {
     console.log('Server iniciado...')
 });
